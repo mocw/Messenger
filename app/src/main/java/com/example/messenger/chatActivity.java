7bdacf27c5f25fb9e1d57618d047e73ab05caf68
wojmo <wojmoc96@gmail.com>
2019-10-11 21:12:29 +0200

    1

--- app/src/main/java/com/example/messenger/chatActivity.java
+++ app/src/main/java/com/example/messenger/chatActivity.java
@@ -1,13 +1,26 @@
 package com.example.messenger;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
+
 public class chatActivity extends AppCompatActivity {
 
     private String user;
+    private DatabaseReference databaseReference;
+    private ProgressDialog progressDialog;
+    private String UID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +30,37 @@ public class chatActivity extends AppCompatActivity {
         Intent i=getIntent();
         user=i.getStringExtra("User");
 
-        Toast.makeText(this, "User: " + user,
-                Toast.LENGTH_LONG).show();
+        progressDialog=new ProgressDialog(chatActivity.this);
+        progressDialog.show();
+        databaseReference= FirebaseDatabase.getInstance().getReference().child("Users");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> items = dataSnapshot.getChildren().iterator();
+                while(items.hasNext()) {
+                    DataSnapshot item = items.next();
+                    String currLogin = item.child("nick").getValue().toString().trim();
+                    if(currLogin.equals(user))
+                    {
+                        UID=item.getKey();
+                        break;
+                    }
+                }
+                progressDialog.dismiss();
+            showData();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
+
+    }
+
+    private void showData() {
 
     }
 }
